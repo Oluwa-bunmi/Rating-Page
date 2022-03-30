90f84988164c800bb38c4f3c77fc02925a51cb57
Jummy007 <gbemisoyeadejumoke@gmail.com>
2022-03-31 00:55:15 +0100

    still on the javascript

---
+++ main.js
@@ -0,0 +1,28 @@
+// document.getElementsByTagName('button').onclick = function(){
+
+// }
+
+
+document.getElementById('one').onclick = function() {
+   
+    document.getElementById('one').style.color = 'orange'
+    // document.getElementById('two').style.backgroundColor = '#4b4b4e'
+    // document.getElementById('three').style.backgroundColor = '#4b4b4e'
+    // document.getElementById('four').style.backgroundColor = '#4b4b4e'
+    // document.getElementById('five').style.backgroundColor = '#4b4b4e'
+    document.getElementById('response').innerHTML = 1;
+    console.log(1);
+
+    
+}
+document.getElementById('two').onclick = function() {
+    document.getElementById('two').style.color = 'orange'
+    // document.getElementById('two').style.backgroundColor = '#4b4b4e'
+    // document.getElementById('three').style.backgroundColor = '#4b4b4e'
+    // document.getElementById('four').style.backgroundColor = '#4b4b4e'
+    // document.getElementById('five').style.backgroundColor = '#4b4b4e'
+    document.getElementById('response').innerHTML = 2;
+    console.log(1);
+
+    
+}
